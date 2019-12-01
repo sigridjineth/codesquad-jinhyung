2e9f9058ba9d8b2df41842317e1f6cc4bf4ff2c0
jypthemiracle <hophfg@yahoo.co.kr>
2019-12-01 20:09:45 +0900

    12/1 1단계 버그수정 및 리팩토링
    구조변경 (리팩토링)
    - 볼, 스트라이크, 안타, 아웃을 확인하는 메소드를 각각 분리하여 gamePlay 함수 크기를 줄였습니다.
    - 새로운 타자를 호출하는 메소드와 타자의 경기결과를 출력하는 메소드가 코드 곳곳에 중복되는 점이 있어, boolean을 활용하여 gamePlay 함수로 통합하여 관리되도록 하였습니다.
    
    버그수정
    - 타자가 아웃된 경우를 출력할 때, 다음 타자로 넘어가기 전에는 볼과 스트라이크가 0이 아님에도 0으로 출력되는 버그를 수정했습니다. (예시: 1S 2B 0O에서 아웃될 시, 출력: 0S 0B 1O)
    - 마지막 타자가 아웃되어 경기가 종료되었음에도 새로운 타자가 호출하는 버그를 수정했습니다.

--- step-1/step1.js
+++ step-1/step1.js
@@ -1,5 +1,6 @@
 //1단계 간단 야구게임 구현하기
 var baseball = {
+    result: ["볼", "스트라이크", "아웃", "안타"], //경우의 수는 4가지이다.
     strikeThreshold: 3, //3번 스트라이크면 아웃이다.
     ballThreshold: 4, //4번 볼이면 1안타가 된다.
     outThreshold: 3, //3번 아웃이면 게임 끝난다.
@@ -8,65 +9,128 @@ var baseball = {
 //새로운 플레이어의 설정
 baseball.player = {
     batterNum: 1, //타자 횟수는 누적값입니다.
+    batterNotice: false,
     ball: 0, //볼은 초기화 대상입니다.
     strike: 0, //스트라이크는 초기화 대상입니다.
     hit: 0, //안타는 누적값입니다.
     out: 0 //아웃은 누적값입니다. 3이 넘어가면 main 함수에서 게임 진행을 종료합니다.
 };
 
-//타석 입장 메시지
-baseball.message = function() {
-    console.log(`${this.player.batterNum} 번째 타자가 타석에 입장했습니다.`);
+//타석 입장 새로운 선수 추가
+baseball.newPlayer = function() {
+    if (baseball.player.out != baseball.outThreshold) {
+        this.player.batterNum++;
+        return baseball.message();
+    };
 };
 
-//실제로 게임을 플레이 해봅니다.
-baseball.gamePlay = function() {
-    var result = ["볼", "스트라이크", "아웃", "안타"] //경우의 수는 4가지이다.
-    var randomNumber = Math.floor(Math.random() * result.length);
-    var thisResult = result[randomNumber];
-    //볼이 4번이면, 볼 횟수를 초기화하고 1안타로 간주합니다.
+baseball.message = function() {
+    return console.log(`${this.player.batterNum} 번째 타자가 타석에 입장했습니다.`);
+};
+
+baseball.determine = function() {
     if (this.player.ball == baseball.ballThreshold) {
         this.player.hit += 1;
         this.player.ball = 0;
+        console.log('4볼이므로 1 안타로 진출합니다.');
+        return true;
     };
-    //스트라이크 3번이면, 스트라이크 횟수를 초기화하고 1아웃으로 간주하고 새 플레이어를 호출합니다.
     if (this.player.strike == baseball.strikeThreshold) {
         this.player.out++;
-        this.player.batterNum++;
+        console.log('3 스트라이크이므로 아웃입니다.');
         this.player.strike = 0;
+        return true;
     };
-    //게임을 진행합니다.
-    if (randomNumber === 0) { //볼인 경우입니다.
-        this.player.ball++;
-    }
-    if (randomNumber === 1) { //스트라이크인 경우입니다.
-        this.player.strike++;
-    }
-    if (randomNumber === 2) { //아웃이 된 경우입니다.
-        this.player.ball = 0;
-        this.player.strike = 0;
-        this.player.out++;
-        this.player.batterNum++;
-    }
-    if (randomNumber === 3) { //안타를 친 경우입니다.
-        this.player.hit++;
-        this.player.strike = 0;
-        this.player.ball = 0;
-    };
-    console.log(baseball.print(result[randomNumber]));
+    return false;
 };
 
+baseball.checkBall = function(randomNumber) {
+    //console.log(`if문 밖의 checkBall입니다. ${randomNumber}`);
+    if (randomNumber === 0) { //볼인 경우입니다.
+        //console.log(`if문 안의 checkBall입니다. ${randomNumber}`);
+        this.player.ball++;
+        return true;
+    };
+    return false;
+};
+
+baseball.checkStrike = function(randomNumber) {
+    //console.log(`if문 밖의 checkStrike입니다. ${randomNumber}`);
+    if (randomNumber === 1) { //스트라이크인 경우입니다.
+        //console.log(`if문 안의 checkStrike입니다. ${randomNumber}`);
+        this.player.strike++;
+        return true;
+    };
+    return false;
+};
+
+baseball.checkOut = function(randomNumber) {
+    //console.log(`if문 밖의 checkOut입니다. ${randomNumber}`);
+    if (randomNumber === 2) { //아웃이 된 경우입니다.
+        //console.log(`if문 안의 checkOut입니다. ${randomNumber}`);
+        this.player.out++;
+        baseball.print(randomNumber);
+        return true;
+    };
+    return false;
+};
+
+baseball.checkHit = function(randomNumber) {
+    //console.log(`if문 밖의 checkHit입니다. ${randomNumber}`);
+    if (randomNumber === 3) { //안타를 친 경우입니다.
+        //console.log(`if문 안의 checkHit입니다. ${randomNumber}`);
+        this.player.hit++;
+        baseball.print(randomNumber);
+        return true;
+    };
+    return false;
+};
+//실제로 게임을 플레이 해봅니다.
+baseball.gamePlay = function(randomNumber) {
+    //console.log(`if문 밖의 gamePlay입니다. ${randomNumber}`);
+    if (baseball.checkBall(randomNumber) || (baseball.checkStrike(randomNumber))) {
+        if (baseball.determine()) {
+            baseball.print(randomNumber);
+            return baseball.newPlayer();
+        }
+        return baseball.print(randomNumber);
+    };
+    if (baseball.checkOut(randomNumber) || baseball.checkHit(randomNumber)) {
+        this.player.ball = 0;
+        this.player.strike = 0;
+        return baseball.newPlayer();
+    };
+};
+
+
+
+//    baseball.checkBall(randomNumber);
+//    baseball.checkStrike(randomNumber);
+
+//    if (baseball.determine() === false) {
+//        console.log(`if문 안의 gamePlay입니다. ${randomNumber}`);
+//        if (baseball.checkOut(randomNumber) === true || baseball.checkHit(randomNumber) === true) {
+//            baseball.print(randomNumber);
+//            baseball.newPlayer();
+//            return;
+//        }
+//    };
+//    return baseball.print(randomNumber);
+//};
+
 //게임 진행상황을 출력하는 함수입니다.
-baseball.print = function(result) {
-    console.log(`${result}! ${baseball.player.strike}S, ${baseball.player.ball}B, ${baseball.player.out}O`);
+baseball.print = function(randomNumber) {
+    return console.log(`${this.player.batterNum}번 선수, ${this.result[randomNumber]}!\n` + `${this.player.strike}S, ${this.player.ball}B, ${this.player.out}O`);
 };
 
 //아웃 여부를 판단하여 게임 진행여부를 판단하는 main 함수를 출력합니다.
 var main = function() {
     console.log('간단하지 않은 야구게임이 시작되었습니다.');
-    while (baseball.player.out < baseball.outThreshold) { //3아웃까지는 계속 플레이 합니다.
-        baseball.message();
-        baseball.gamePlay();
+    baseball.message();
+    while (baseball.player.out < baseball.outThreshold) {
+        var randomNumber = Math.floor(Math.random() * baseball.result.length);
+        //console.log(`${randomNumber} main 함수의 randomNumber가 생성되었습니다.`)
+        baseball.gamePlay(randomNumber);
     };
     if (baseball.player.out == baseball.outThreshold) { //3아웃이 넘어가면 게임을 종료합니다.
         console.log(`최종 안타수: ${baseball.player.hit}`);
